f63d462447c214ac96557e20ed9bc13865d0e399
Gaurav Saini <gsaini313@gmail.com>
2022-04-26 00:12:58 -0400

    Fresh Api

---
+++ Controllers/Ephemeral.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using YamlDotNet.Serialization;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MicroService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class Ephemeral : ControllerBase
+    {
+        // GET: api/<EphemeralController>
+        [HttpGet]
+        public object Get()
+        {
+            var yamlfile = new StreamReader("ConfigurationSettings\\Ephemeral\\provision_ephemeral_config.yml");
+            var yaml = new Deserializer();
+            var Resultobject = yaml.Deserialize(yamlfile);
+
+            return Resultobject;
+        }
+    }
+}
---
+++ Controllers/Perpetual.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using YamlDotNet.Serialization;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MicroService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class Perpetual : ControllerBase
+    {
+        // GET: api/<Perpetual>
+        [HttpGet]
+        public object Get()
+        {
+            var yamlfile = new StreamReader("ConfigurationSettings\\Perpetual\\provision_perpetual_config.yml");
+            var yaml = new Deserializer();
+            var Resultobject = yaml.Deserialize(yamlfile);
+
+            return Resultobject;
+        }
+    }
+}
